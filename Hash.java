a3e126aabd248dd2552bcbdadc6cdb61baac2bc8
bkdavenport <bdavenport@mail.bradley.edu>
2018-03-04 22:48:42 -0600

    Update Hash.java

--- Hash.java
+++ Hash.java
@@ -1,19 +1,19 @@
-package digitalsignaturesolution;
-
 /**
- *
- * Brandon Davenport
- */
+*<p>Cryptography - Digital Signature
+* This is my hash function that mods the value of message.
+*
+*
+*@author Brandon Davenport
+*@date 03/02/2018
+*/
+package digitalsignaturesolution;
 import java.math.BigInteger;
-
 public class Hash {
-	
+
 	public BigInteger Hashing(BigInteger message)
         {
-    	BigInteger modulus = new BigInteger("8");
-    	BigInteger hVal = message.mod(modulus);
+    	BigInteger modulus = new BigInteger("5"); // Takes the input within sender and divides it by 5
+    	BigInteger hVal = message.mod(modulus); 
     	return hVal;
     	}
-    
-
 }
