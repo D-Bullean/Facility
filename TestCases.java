e7d31ab93bff14ee16e245663d450f6adc66e888
Dittymaster <36720788+Dittymaster@users.noreply.github.com>
2018-03-04 22:57:13 -0600

    Add files via upload

--- TestCases.java
+++ TestCases.java
@@ -10,6 +10,7 @@ public class TestCases {
 		blockCipher _block = new blockCipher();
 		CBC _cbc = new CBC();
 		MAC _mac = new MAC();
+		CA _ca = new CA();
 
 		int[] shifts = { 1, 2, 5 };
 		// Shift Cipher Testing
@@ -23,9 +24,8 @@ public class TestCases {
 		System.out.println(_poly.decrypt("Igqmq Xqwmf", shifts));
 		// RSA Cipher Testing
 		BigInteger message = new BigInteger("4");
-		BigInteger m = _rsa.encrypt(message);
-		BigInteger n = _rsa.decrypt(_rsa.encrypt(message));
-		System.out.println(m.toString());
+		System.out.println(_rsa.encrypt(message));
+		// System.out.println(m.toString());
 		// Block Cipher Testing *currently only works with 3 as a key size*
 		_block.encrypt("010110001111", 3);
 		_block.decrypt("101000111001", 3);
@@ -36,7 +36,8 @@ public class TestCases {
 		MAC_package pack = new MAC_package();
 		pack = _mac.encrypt(message, message);
 		System.out.println("\nMessage Authenticated:" + _mac.verify(message, pack));
-
-		//
+		// CA Testing
+		System.out.println(_ca.authorize("8675309"));
+		System.out.println(_ca.certify("8675309", "1234"));
 	}
 }
