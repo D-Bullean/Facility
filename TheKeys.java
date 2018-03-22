834e2318e578ac389a608974d164b6b985b6f6ad
bkdavenport <bdavenport@mail.bradley.edu>
2018-03-22 15:12:37 -0500

    Update TheKeys.java

--- TheKeys.java
+++ TheKeys.java
@@ -10,14 +10,14 @@ package digitalsignaturesolution;
 public class TheKeys  //Begins the RSA algorithm for the Sender and Receiver. 
 {
 
-	RSA sender = new RSA();
+	RSA Equations = new RSA();
 	RSA receiver = new RSA();
 	
 	
 	public TheKeys(){
-		sender.generateKeys();
-		System.out.println("Sender's public key: John(n, e) = (" + sender.getN() + "," + sender.getE() + ")"); // Gives the Sender's Public Key 
-		System.out.println("Sender's private key: John(n, d) = (" + sender.getN() + "," + sender.getD() + ")"); // Gives the Sender's Private Key
+		Equations.generateKeys();
+		System.out.println("Sender's public key: John(n, e) = (" + Equations.getN() + "," + Equations.getE() + ")"); // Gives the Sender's Public Key 
+		System.out.println("Sender's private key: John(n, d) = (" + Equations.getN() + "," + Equations.getD() + ")"); // Gives the Sender's Private Key
 		receiver.generateKeys();
 		System.out.println("Receiver's public key:Jane(n, e) = (" + receiver.getN() + "," + receiver.getE() +")");//Gives Reciever Private Key.
 		System.out.println("Receiver's private key:Jane(n, d) = (" + receiver.getN() + "," + receiver.getD() +")" + "\n" ); //Gives Receiever's Private Key
