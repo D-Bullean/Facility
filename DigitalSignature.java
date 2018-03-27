a89f9dbe2ba9bf05170284e3b45efcff254ecc64
TyFighter297 <31391209+TyFighter297@users.noreply.github.com>
2018-03-26 20:08:16 -0500

    Add files via upload

--- DigitalSignature.java
+++ DigitalSignature.java
@@ -1,89 +1,41 @@
 /**
 *<p>Cryptography - Digital Signature
-*This is the methodology behind Digital Signature. Using the session key, message, and the signature, each returns
-* something to be placed with the changing variables with different inputs.
+*The digital signature hashes the message
+*Then signs it with the sender's private key
 *
-*@author Brandon Davenport
-*@date 03/02/2018
+*@author Tyler Connors
+*@date 03/26/2018
 */
-package digitalsignaturesolution;
+
 import java.math.BigInteger;
+import java.util.Scanner;
 
-public class DigitalSignature //Method for Session Key, Message, and the signature.
+public class DigitalSignature
 {
-
-    private BigInteger SKey;
-    private BigInteger message;
-    private BigInteger signature;
-
-    DigitalSignature()  // Sets all to BigInteger
-    {
-
-        SKey = BigInteger.ZERO;
-        message = BigInteger.ZERO;
-        signature = BigInteger.ZERO;
-
-    }
-
-    public DigitalSignature(BigInteger SKey, BigInteger message, BigInteger signature) 
-    {
-        this.SKey = SKey;
-        this.message = message;
-        this.signature = signature;
-    }
-
-    
-    public BigInteger getSessionKey() //Gets the Session Key
-    {
-        return SKey;
-    }
-   
-  
-    public void setSessionKey(BigInteger sessionKey) 
-    {
-        this.SKey = sessionKey;
-    }
-
-   
-    public BigInteger getMessage() //Grabs the message.
-    {
-        return message;
-    }
-
-    public void setMessage(BigInteger message) 
-    {
-        this.message = message;
-    }
-
-    public void setErrorInMessage(BigInteger error)
-    {
-        this.message = this.message.add(error);
-    }
-
-    
-    public BigInteger getSignature() //Grabs the signature 
-    {
-        return signature;
-    }
-
-    
-    public void setSignature(BigInteger signature) 
-    {
-        this.signature = signature;
-    }
-
-    @Override
-	public String toString() //Shows the Message, Signature and Session Key. 
-        {
-
-        String result = " ";
-
-        result = "Message = " + this.message.toString() + "\n"
-                + "Signature = " + this.signature.toString() + "\n"
-                + "SessionKey = " + this.SKey.toString() + "\n";
-
-        return result;
-    }
-
+	//Hashes the message
+	public BigInteger hash(BigInteger m)
+	{
+		return m.mod(BigInteger.valueOf(13));
+	}
+	
+	//Uses private RSA key of sender to to encrypt hashed message
+	public BigInteger generateDigSig(BigInteger m)
+	{
+		BigInteger result;
+		RSA senderKeys = new RSA();
+		senderKeys.init();
+		BigInteger hashed = hash(m);
+		result = senderKeys.privEncrypt(hashed);
+		return result;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		DigitalSignature dig = new DigitalSignature();
+		System.out.println("Enter test message");
+		BigInteger m = scan.nextBigInteger(); //Generates test m value
+		System.out.println("Original message " + m);
+		System.out.println("Signed " + dig.generateDigSig(m));
+	}
 }
-
