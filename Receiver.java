489f350c5b0baeaf64baacd35b045044508ad877
Dittymaster <36720788+Dittymaster@users.noreply.github.com>
2018-03-30 21:28:41 -0500

    updated

--- Receiver.java
+++ Receiver.java
@@ -4,27 +4,67 @@ public class Receiver {
 	Package pack;
 
 	public Receiver() {
-		System.out.println("Receiver created");
+		System.out.println("\nReceiver created");
 		pack = new Package();
+		RSA key;
+
 	}
 
-	public void receive(Network network, RSA receiver) {
+	public void receive(Network network, RSA_Keys key, int methodType) {
 
-		System.out.println("Receive packet from internet");
+		ShiftCipher _shift = new ShiftCipher();
+		blockCipher _block = new blockCipher();
+		System.out.println("\n___Receive packet from internet___");
 		pack = network.getPacketFromSender();
 
-		System.out.println("Split Packet");
+		System.out.print("\n___Split Packet___");
 		BigInteger message = pack.getMessage();
 		BigInteger sessionKey = pack.getSessionKey();
 		BigInteger signature = pack.getSignature();
+		System.out.println(pack.toString());
 
-		System.out.println("Decrypting the session key with Receivers Private");
-		BigInteger decryptSession = receiver.privDecrypt(sessionKey);
+		System.out.println("\n___Decrypting the session key with Receivers Private___");
+		BigInteger decryptSession = key.receiver.privDecrypt(sessionKey);
 		System.out.println("E-Session Key:" + sessionKey);
 		System.out.println("D-Session Key:" + decryptSession);
 
-		System.out.println("Decrypt message using decrypted session key");
+		System.out.println("\n___Decrypt message using decrypted session key___");
+		BigInteger decryptMessage = BigInteger.ZERO;
+		BigInteger decryptSignature = BigInteger.ZERO;
+		if (methodType == 0) {
 
+			decryptMessage = _shift.decrypt(message, decryptSession);
+		} else if (methodType == 1) {
+
+			decryptMessage = new BigInteger(_block.decrypt(message.toString(), 3));
+		}
+		System.out.println("Decrypted message:" + decryptMessage);
+
+		System.out.println("\n___Decrypt Signature with session___");
+		if (methodType == 0) {
+			decryptSignature = _shift.decrypt(signature, decryptSession);
+		} else if (methodType == 1) {
+			decryptSignature = new BigInteger(_block.decrypt(signature.toString(), 3));
+		}
+
+		System.out.println("Signature:" + decryptSignature);
+
+		System.out.println("\n___Hash Message___");
+		DigitalSignature digSignature = new DigitalSignature();
+		BigInteger hashedMessage = digSignature.hash(decryptMessage);
+		System.out.println("Hashed Message:" + hashedMessage);
+
+		System.out.println("\n___Decrypt Signature with Senders Public___");
+		BigInteger decryptPubSign = key.sender.pubEncrypt(decryptSignature);
+		System.out.println("Decrypted Signature:" + decryptPubSign);
+
+		System.out.println("\n___Comparing Hash___");
+		System.out.println(hashedMessage);
+		System.out.println(decryptPubSign);
+		if (hashedMessage.equals(decryptPubSign)) {
+			System.out.println("No errors");
+		} else
+			System.out.println("Error in Packet!");
 	}
 
 }
--- Sender.java
+++ Sender.java
@@ -3,50 +3,56 @@ import java.math.BigInteger;
 public class Sender {
 	private Package pack;
 	RSA_Keys key;
+	ShiftCipher _shift = new ShiftCipher();
+	blockCipher _block = new blockCipher();
 
 	public Sender(RSA_Keys key, int methodType) {
-		System.out.println("Sender created...");
+		System.out.println("\n___Sender created___");
 		System.out.println("Generate Message");
-		BigInteger message = new BigInteger("70");
+		BigInteger message = new BigInteger("89");
 		System.out.println("Message:" + message);
 
-		System.out.println("Hashing Message");
+		System.out.println("\n___Hashing Message___");
 		DigitalSignature hasher = new DigitalSignature();
 		BigInteger hashMessage = hasher.hash(message);
 		System.out.println("Hashed Message:" + hashMessage);
 
-		System.out.println("Sign H(m) with Senders Private Key");
-		BigInteger digSignature = hasher.generateDigSig(message, key.sender);
+		System.out.println("\n___Sign H(m) with Senders Private Key___");
+		BigInteger digSignature = key.sender.privEncrypt(hashMessage);
 		System.out.println("Digital Signature:" + digSignature);
 
-		System.out.println("Generate Session Key");
+		System.out.println("\n___Generate Session Key___");
 		BigInteger symmetricKey = new BigInteger("5");
 		System.out.println("Session Key:" + symmetricKey);
 
-		System.out.println("Encrypt Session with Receivers Public Key");
+		System.out.println("\n___Encrypt Session with Receivers Public Key___");
 		BigInteger encryptedSymmetric = key.receiver.pubEncrypt(symmetricKey);
 		System.out.println("Encrypted Session Key:" + encryptedSymmetric);
 
-		System.out.println("Encrypt message with symmetric key");
-		System.out.println("Encrypt Digital Signature with Symmetric Key");
+		System.out.println("\n____Encrypt message with symmetric key___");
+		System.out.println("___Encrypt Digital Signature with Symmetric Key___");
 		BigInteger encryptedMessage = BigInteger.ZERO;
 		BigInteger encryptedSig = BigInteger.ZERO;
 		if (methodType == 0) {
-			ShiftCipher _shift = new ShiftCipher();
-			encryptedMessage = new BigInteger(_shift.encrypt(message.toString(), symmetricKey.intValue()));
-			encryptedSig = new BigInteger(_shift.encrypt(digSignature.toString(), symmetricKey.intValue()));
+
+			encryptedMessage = _shift.encrypt(message, symmetricKey);
+			encryptedSig = _shift.encrypt(digSignature, symmetricKey);
 
 		} else if (methodType == 1) {
-			blockCipher _block = new blockCipher();
+
 			encryptedMessage = new BigInteger(_block.encrypt(message.toString(), 3));
 			encryptedSig = new BigInteger(_block.encrypt(digSignature.toString(), 3));
 		}
 		System.out.println("Encrypted Message:" + encryptedMessage);
 		System.out.println("Encrypted Signature:" + encryptedSig);
 
-		System.out.println("Creating Packet");
+		System.out.print("\n___Creating Packet___");
 		pack = new Package(encryptedSymmetric, encryptedMessage, encryptedSig);
-		System.out.println("Packet info:" + pack.toString());
+		System.out.println(pack.toString());
 
+	}
+
+	public Package getPacket() {
+		return pack;
 	}
 }
